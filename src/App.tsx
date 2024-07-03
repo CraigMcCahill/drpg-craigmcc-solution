60fa260fb23a5025105e5318f990103a6238a0d9
Paul Stevenson <paul.stevenson@drpgroup.com>
2024-07-03 16:48:54 +0100

    Frontend coding test

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className="App-header">
+        <img
+          src={"https://www.drpgroup.com/hubfs/drpg-blue-logo.png"}
+          className="App-logo"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
