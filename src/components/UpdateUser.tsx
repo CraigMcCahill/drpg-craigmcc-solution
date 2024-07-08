e4133dafe6aa6dffde09917bf4f4d8a4adebe793
Craig McCahill <craig.mccahill@gmail.com>
2024-07-08 14:37:29 +0100

    fix: Missing types for react modal

--- src/components/UpdateUser.tsx
+++ src/components/UpdateUser.tsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState, useMemo } from 'react';
+import Modal from 'react-modal';
 import { User } from './Dashboard';
 
 interface UpdateUserProps {
   user: User;
+  isOpen: boolean;
 }
 
-function UpdateUser({ user }: UpdateUserProps) {
-  return (<form></form>)
+function UpdateUser({ user, isOpen }: UpdateUserProps) {
+  const [editUser, setEditUser] = useState<boolean>(false);
+
+  return (
+    <Modal
+    contentLabel="Update User Details"
+    isOpen={isOpen}
+  >
+    <form></form>
+  </Modal>)
 }
 
 export default UpdateUser;
