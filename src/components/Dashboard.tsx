d2221c41dfa7cb0e569449f2b0906637f1e1cca0
Craig McCahill <craig.mccahill@gmail.com>
2024-07-08 14:05:21 +0100

    Add simple search but only on each page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState, useMemo } from 'react';
-
 import DataTable from 'react-data-table-component';
+import { API_URL } from '../constants';
 
-const API_URL = 'https://reqres.in/api/users';
-
-interface User {
+export interface User {
   id: number;
   email: string;
   first_name: string;
@@ -12,7 +10,7 @@ interface User {
   avatar: string;
 }
 
-interface Users {
+export interface Users {
   page: number;
   per_page: number;
   total: number;
@@ -21,11 +19,13 @@ interface Users {
 }
 
 function Dashboard() {
-  const [data, setData] = useState<User[]>([]);
+  const [data, setData] = useState<Array<User>>([]);
+  const [filteredData, setFilteredData] = useState<Array<User>>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [totalPages, setTotalPages] = useState<number>(1);
-  const [currentPage,setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,6 +36,7 @@ function Dashboard() {
         }
         const result: Users = await response.json();
         setData(result.data);
+        setFilteredData(result.data);
         setTotalPages(result.total_pages);
       } catch (error: any) {
         setError(error.message);
@@ -47,31 +48,39 @@ function Dashboard() {
     fetchData();
   }, [currentPage]);
 
+  useEffect(() => {
+    const filtered = data.filter((user) =>
+      user.last_name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredData(filtered);
+  }, [searchQuery, data]);
+
   const columns = useMemo(
-		() => [
-			{
-				name: 'ID',
-				selector: (row: User) => row.id,
-				sortable: true,
-			},
-			{
-				name: 'Email',
-				selector: (row: User) => row.email,
-				sortable: true,
-			},
+    () => [
       {
-				name: 'First Name',
-				selector: (row: User) => row.first_name,
-				sortable: true,
-			},
-			{
-				name: 'Last Name',
-				selector: (row: User) => row.last_name,
-				sortable: true,
-			},
-		],
-		[],
-	);
+        name: 'ID',
+        selector: (row: User) => row.id,
+        sortable: true,
+      },
+      {
+        name: 'Email',
+        selector: (row: User) => row.email,
+        sortable: true,
+      },
+      {
+        name: 'First Name',
+        selector: (row: User) => row.first_name,
+        sortable: true,
+      },
+      {
+        name: 'Last Name',
+        selector: (row: User) => row.last_name,
+        sortable: true,
+      },
+    ],
+    [],
+  );
 
   if (loading) {
     return <p>Loading...</p>;
@@ -82,27 +91,33 @@ function Dashboard() {
   }
 
   return (
-  <div>
-    <DataTable 
-      title="Users"
-      data={data}
-      columns={columns}
-    />
     <div>
-      <button
-        onClick={() => setCurrentPage(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        Previous
-      </button>
-      <button
-        onClick={() => setCurrentPage(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        Next
-      </button>
+      <input
+        type="search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search by last name, or email"
+      />
+      <DataTable
+        title="Users"
+        data={filteredData}
+        columns={columns}
+      />
+      <div>
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
-  </div>
   );
 }
 
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+// In a real application this would be stored in .env file that would not be committed
+export const API_URL = 'https://reqres.in/api/users';
