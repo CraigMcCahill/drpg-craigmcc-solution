0c42b2de62387344adc0e1786cb1481e9b104801
Craig McCahill <craig.mccahill@gmail.com>
2024-07-08 14:23:04 +0100

    feat: create skeleton update user form

---
+++ src/components/UpdateUser.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import { User } from './Dashboard';
+
+interface UpdateUserProps {
+  user: User;
+}
+
+function UpdateUser({ user }: UpdateUserProps) {
+  return (<form></form>)
+}
+
+export default UpdateUser;
